5148d25431b3be677eb5b2ec337f6f514ea4cf33
SakeXYZ <islamshamshiev2002@gmail.com>
2024-11-25 20:09:38 +0600

    Добавлен в класс WomanSerializer

--- drfsite/woman/views.py
+++ drfsite/woman/views.py
@@ -11,17 +11,21 @@ from .serializer import WomanSerializer
 
 class WomanAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        return Response({'posts': WomanSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomanSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
-            title = request.data['title'],
-            content = request.data['content'],
-            cat_id = request.data['cat_id'],
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
 
         )
-        return Response({'post': model_to_dict(post_new)})
+
+        return Response({'post': WomanSerializer(post_new).data})
 
 # class WomanAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
